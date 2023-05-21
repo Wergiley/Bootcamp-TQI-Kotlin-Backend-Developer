c35cd4e2066c905b667dd3f39fb7682ea678bb01
Wergiley <werslley.robson@gmail.com>
2023-05-21 12:20:10 -0300

    att desafio

--- aprenda-kotlin-com-exemplos-main/desafio.kt
+++ aprenda-kotlin-com-exemplos-main/desafio.kt
@@ -3,55 +3,34 @@
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
+
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class FormacaoEducacional(val nome: String) {
-    private val conteudos = mutableListOf<ConteudoEducacional>()
-    private val inscritos = mutableListOf<Usuario>()
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    fun adicionarConteudo(conteudo: ConteudoEducacional) {
-        conteudos.add(conteudo)
-    }
-
+    val inscritos = mutableListOf<Usuario>()
+    
     fun matricular(usuario: Usuario) {
         inscritos.add(usuario)
-    }
-
-    fun listarConteudos(): List<ConteudoEducacional> {
-        return conteudos
-    }
-
-    fun listarInscritos(): List<Usuario> {
-        return inscritos
+    	println("Usuário ${usuario} matriculado na formação ${nome}.")
     }
 }
 
 fun main() {
-    // Criar usuários
-    val usuario1 = Usuario("1", "João")
-    val usuario2 = Usuario("2", "Maria")
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
 
-    // Criar conteúdos educacionais
-    val conteudo1 = ConteudoEducacional("Introdução à Programação", 120)
-    val conteudo2 = ConteudoEducacional("Estruturas de Dados", 180)
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 90)
+    val conteudo2 = ConteudoEducacional("Estruturas de Dados", 120)
+    val conteudo3 = ConteudoEducacional("Algoritmos Avançados", 150)
 
-    // Criar formação educacional
-    val formacao = FormacaoEducacional("Ciência da Computação")
-    formacao.adicionarConteudo(conteudo1)
-    formacao.adicionarConteudo(conteudo2)
+    val formacao = Formacao("Ciência da Computação", listOf(conteudo1, conteudo2, conteudo3))
 
-    // Matricular usuários na formação
     formacao.matricular(usuario1)
     formacao.matricular(usuario2)
 
-    // Exibir informações da formação
-    println("Formação: ${formacao.nome}")
-    println("Conteúdos:")
-    for (conteudo in formacao.listarConteudos()) {
-        println("- ${conteudo.nome}")
-    }
-    println("Inscritos:")
-    for (usuario in formacao.listarInscritos()) {
-        println("- ${usuario.nome}")
+    println("Usuários inscritos na formação ${formacao.nome}:")
+    for (usuario in formacao.inscritos) {
+        println(usuario)
     }
 }
